7e9da987862784e3aa793ad1fcfbd2010ab9c2c1
Gustavo Medori <gustavo.medori@goteleport.com>
2026-07-09 16:32:28 -0600

    Track Neovim plugin versions

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,26 @@
+local selected = "badwolf"
+
 return {
 	{
 		"neanias/everforest-nvim",
 		version = false,
-		lazy = false,
+		lazy = selected ~= "everforest",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		-- Optional; default configuration will be used if setup isn't called.
 		config = function()
-			require("everforest").load() -- Enable the colorscheme
+			if selected == "everforest" then
+				require("everforest").load() -- Enable the colorscheme
+			end
 		end,
-	}
+	},
+	{
+		"sjl/badwolf",
+		lazy = selected ~= "badwolf",
+		priority = 1000,
+		config = function()
+			if selected == "badwolf" then
+				vim.cmd.colorscheme("badwolf")
+			end
+		end,
+	},
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return { -- Fuzzy Finder (files, lsp, etc)
 	'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
+	version = '*',
 	event = 'VimEnter',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
